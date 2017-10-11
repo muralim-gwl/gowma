7cb4215f84c2ef9ec847ef26905121dba65c5e6c
Murali <muralim4242@users.noreply.github.com>
2017-10-11 22:43:25 +0530

    Updated app, header and view of product

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 // import $ from "jquery";
 // import * as actions from '../actions';
@@ -12,22 +12,23 @@ import ViewProdcut from './content/ViewProduct';
 
 // import Dashboard from './Dashboard';
 // import SurveyNew from './surveys/SurveyNew';
+var basePath=""
 
 class App extends Component {
 
   render() {
     return (
       <div className="container-fluid">
-        <BrowserRouter>
+        <HashRouter>
           <div>
             <Header />
               <br/>
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/view-product" component={ViewProdcut} />
+                <Route exact path={basePath+"/"} component={Landing} />
+                <Route exact path={basePath+"/view-product"} component={ViewProdcut} />
               <br/>
             <Footer/>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -15,15 +15,15 @@ class Header extends Component {
     return (
       <div className="navbar-fixed">
           <ul id="dropdown1" className="dropdown-content">
-            <li><a href="#!">Your Account</a></li>
-            <li><a href="#!">Your Order</a></li>
+            <li><a href="#">Your Account</a></li>
+            <li><a href="#">Your Order</a></li>
             <li className="divider"></li>
-            <li><a href="#!">Login</a></li>
-            <li><a href="#!">Register</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
          </ul>
           <nav>
             <div className="nav-wrapper">
-              <a href="#!" className="brand-logo"><i className="material-icons">polymer</i>Gowma</a>
+              <a href="#" className="brand-logo"><i className="material-icons">polymer</i>Gowma</a>
               <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
               <ul className="right hide-on-med-and-down">
                 <li>
@@ -34,12 +34,12 @@ class Header extends Component {
                 </li>
                 <li><a href="#"><i className="material-icons left">search</i></a></li>
                 <li><a href="#"><i className="material-icons left">add_shopping_cart</i>Cart<span className="new badge">0</span></a></li>
-                <li><a className="dropdown-button" href="#!" data-activates="dropdown1"><i className="material-icons left">account_box</i>Sign In<i className="material-icons right">arrow_drop_down</i></a></li>
+                <li><a className="dropdown-button" href="#" data-activates="dropdown1"><i className="material-icons left">account_box</i>Sign In<i className="material-icons right">arrow_drop_down</i></a></li>
               </ul>
 
               <ul className="side-nav" id="mobile-demo">
                 <li><a href="#"><i className="material-icons left">add_shopping_cart</i>Cart<span className="new badge">0</span></a></li>
-                
+
               </ul>
             </div>
           </nav>
